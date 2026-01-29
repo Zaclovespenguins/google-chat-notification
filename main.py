ca8747cf62e6f0e908dbec92af32d448b69bc4aa
Zaclovespenguins <46504778+Zaclovespenguins@users.noreply.github.com>
2026-01-29 14:18:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def run():
 
     # Creating body based on number of commits
     if len(commits) == 1:
-        title = f"{name} pushed a new commit"
+        title = f"{name} pushed a new commit \n"
     else:
-        title = f"{name} pushed {len(commits)} new commits"
+        title = f"{name} pushed {len(commits)} new commits \n"
 
     for commit in commits:
         body += f"Commit [{commit['id'][-7:]}]({commit['url']}) - {commit['message']} \n"
@@ -36,7 +36,7 @@ def run():
             {
                 "cardId": unique_id,
                 "card": {
-                    "header": {"title": f"{title}"},
+                    "header": {"title": f"**{title}**"},
                     "sections": [
                         {
                             "header": "Commit Message",
